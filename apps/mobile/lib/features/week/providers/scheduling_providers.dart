ae19d1dabe867ac25649a5f9686315941be2a22e
Hermann <99550985+Hermann-33@users.noreply.github.com>
2026-08-10 01:20:55 +0800

    TASK-IMP-004: Add mobile scheduling providers

---
+++ apps/mobile/lib/features/week/providers/scheduling_providers.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:stone_set_data/stone_set_data.dart';
+import 'package:stone_set_domain/scheduling.dart';
+
+import '../../identity/providers/identity_providers.dart';
+
+final schedulingRepositoryProvider = Provider<SchedulingRepository>((ref) {
+  final client = ref.watch(supabaseClientProvider);
+  return SupabaseSchedulingRepository(remote: SupabaseSchedulingRemoteService(client));
+});
+
+final currentWeekProvider = FutureProvider.autoDispose<WeekLoadResult>((ref) {
+  return ref.watch(schedulingRepositoryProvider).getOrCreateCurrentWeek();
+});

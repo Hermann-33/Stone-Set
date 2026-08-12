4fba873e906194fd8d683f884e59f524ea17bf0a
Hermann <99550985+Hermann-33@users.noreply.github.com>
2026-08-12 19:42:43 +0800

    TASK-IMP-013A: synchronize cached shell on startup and resume

--- apps/mobile/lib/features/shell/views/mobile_authenticated_shell.dart
+++ apps/mobile/lib/features/shell/views/mobile_authenticated_shell.dart
@@ -1,9 +1,14 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../identity/controllers/mobile_session_controller.dart';
+import '../../sync/controllers/mobile_sync_controller.dart';
 import '../models/mobile_destination.dart';
 
-class MobileAuthenticatedShell extends StatelessWidget {
+class MobileAuthenticatedShell extends ConsumerStatefulWidget {
   const MobileAuthenticatedShell({
     required this.navigationShell,
     super.key,
@@ -12,7 +17,44 @@ class MobileAuthenticatedShell extends StatelessWidget {
   final StatefulNavigationShell navigationShell;
 
   @override
+  ConsumerState<MobileAuthenticatedShell> createState() => _MobileAuthenticatedShellState();
+}
+
+class _MobileAuthenticatedShellState extends ConsumerState<MobileAuthenticatedShell>
+    with WidgetsBindingObserver {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      unawaited(_synchronize(MobileSyncTrigger.startup));
+    });
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      unawaited(_synchronize(MobileSyncTrigger.resume));
+    }
+  }
+
+  Future<void> _synchronize(MobileSyncTrigger trigger) async {
+    final ownerId = ref.read(mobileSessionControllerProvider).value?.userId;
+    if (ownerId == null) return;
+    final coordinator = ref.read(mobileSyncControllerProvider.notifier);
+    await coordinator.initializeForOwner(ownerId);
+    await coordinator.synchronize(trigger: trigger);
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final navigationShell = widget.navigationShell;
     return Scaffold(
       body: navigationShell,
       bottomNavigationBar: NavigationBar(
